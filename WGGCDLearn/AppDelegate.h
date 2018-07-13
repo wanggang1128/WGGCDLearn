88a52f8abff511b39aff4bac966885ff56f90c3f
wanggang <978159546@qq.com>
2018-07-13 16:05:50 +0800

    Initial Commit

---
+++ WGGCDLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGGCDLearn
+//
+//  Created by wanggang on 2018/7/13.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGGCDLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGGCDLearn
+//
+//  Created by wanggang on 2018/7/13.
+//  Copyright © 2018年 wanggang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
